d3c9b5a8aaa4ba191f977c4a854c99e4296c1f34
sabbir2809 <sabbirto13@gmail.com>
2024-02-26 17:46:54 +0600

    [Features]: User Registration, Login, Profile View or Update and Reset Password

--- backend/src/services/user.service.js
+++ backend/src/services/user.service.js
@@ -1,7 +1,9 @@
 const config = require("../config");
 const AppError = require("../errors/AppError");
-const User = require("../models/user");
+const User = require("../models/user.modal");
+const OTP = require("../models/opt.modal");
 const { comparePassword, createToken, hashPassword } = require("../utils/helper");
+const { sendEmail } = require("../utils/sendEmail");
 
 // create user
 const createUserIntoDB = async (payload) => {
@@ -77,9 +79,62 @@ const userUpdateFromDB = async (email, payload) => {
   return result;
 };
 
+// Verify Email
+const verifyEmail = async (email) => {
+  // 6 digits OTP Generate
+  const OTP = Math.floor(100000 + Math.random() * 900000);
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new AppError(404, "This User is Not Found!");
+  }
+  // create OTP
+  await OTP.create({ email, otp: OTP });
+
+  // email format & send email with nodemailer
+  const emailData = {
+    email,
+    subject: "Inventory Management System",
+    html: `
+        <p>Hi, ${email}</p>
+        <h1>Your Verify OTP Code: ${OTP}</h1>
+      `,
+  };
+  await sendEmail(emailData);
+};
+
+// Verify OTP
+const verifyOTP = async (email, otp) => {
+  const userOTP = await OTP.findOne({ email, otp, status: 0 });
+  if (!userOTP) {
+    throw new AppError(404, "OTP Code Already Used");
+  }
+
+  await OTP.updateOne({ email, otp, status: 0 }, { email, otp, status: 1 }, { upsert: true });
+  return null;
+};
+
+// Reset Password
+const resetPassword = async (payload) => {
+  const { email, otp, newPassword } = payload;
+
+  const userOTP = await OTP.findOne({ email, otp, status: 1 });
+  if (!userOTP) {
+    throw new AppError(404, "Invalid Email or Password");
+  }
+  // Hashing Password before store user data
+  const hashedPassword = await hashPassword(newPassword);
+  await User.updateOne({ email }, { password: hashedPassword });
+
+  return null;
+};
+
 module.export = {
   createUserIntoDB,
   userLogin,
   userDetailsFromDB,
   userUpdateFromDB,
+  verifyEmail,
+  verifyOTP,
+  resetPassword,
 };
